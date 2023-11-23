679cdca8e471046557c0cf424f6661276a505415
slyvka800 <p.a.slyvka@gmail.com>
2023-11-23 15:45:18 +0100

    merging values

--- acceleration_measurement.py
+++ acceleration_measurement.py
@@ -4,7 +4,15 @@ import tempfile
 import time
 
 def get_i2c(reg: str):
-    os.system("i2cget 1 0x68 " + reg)
+    os.system("i2cget -y 1 0x68 " + reg)
+
+def read_i2c() -> bytes:
+    with tempfile.TemporaryFile() as tempf:
+            proc = subprocess.Popen(['echo'], stdout=tempf)
+            proc.wait()
+            tempf.seek(0)
+            bytes_arr = tempf.read()
+            return bytes_arr[0]
 
 if __name__ == "__main__":
 
@@ -12,11 +20,32 @@ if __name__ == "__main__":
 
         get_i2c('0x3B')
 
-        with tempfile.TemporaryFile() as tempf:
-            proc = subprocess.Popen(['echo'], stdout=tempf)
-            proc.wait()
-            tempf.seek(0)
-            bytes_arr = tempf.read()
-            print(bytes_arr[0])
+        x1 = read_i2c()
+
+        # time.sleep(0.05)
+
+        get_i2c('0x3C')
+
+        x2 = read_i2c()
+
+        final_value = (x1 << 8) | x2
+
+        print(final_value)
 
         time.sleep(0.05)
+
+
+        # get_i2c('0x3D')
+
+        # y1 = read_i2c()
+
+        # time.sleep(0.05)
+
+        # get_i2c('0x3E')
+
+        # y2 = read_i2c()
+
+        # time.sleep(0.05)
+
+
+        
